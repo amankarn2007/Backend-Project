71b30acb6a2b1414bbfaad1efd1e236b2608bf98
Aman Karn <amankarn2007@gmail.com>
2026-01-03 12:23:50 +0530

    security increased

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -2,7 +2,12 @@ const jwt = require("jsonwebtoken");
 
 //genrateRoken me ham user ya admin ko bhejenge, aur ye email aur userId se token gerate karega
 const generateToken = function(user){
-    return jwt.sign({email: user.email, id: user._id}, process.env.JWT_KEY);
+    return jwt.sign(
+        {email: user.email, id: user._id},
+        process.env.JWT_KEY,
+        {expiresIn: "7d"},
+    );
 }
+// {expiresIn: "7d"} applies jwt token expiry date, that secure our token
 
 module.exports.generateToken = generateToken;
